05eedaa8d7afdb645dc5f3c605c3938f9f04baf1
magwindow <antosha.petrakov@yandex.ru>
2025-10-30 23:57:26 +0300

    sort3

--- sort3.py
+++ sort3.py
@@ -1,4 +1,6 @@
+import math
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 def insert_cabinet(cabinet,to_insert):
@@ -24,13 +26,6 @@ def insertion_sort(cabinet):
         newcabinet = insert_cabinet(newcabinet,to_insert)
     return newcabinet
 
-
-cabinet = [8,4,6,1,2,5,3,7]
-stepcounter = 0
-sortedcabinet = insertion_sort(cabinet)
-# print("Кол-во шагов:", stepcounter)
-
-
 def check_steps(size_of_cabinet):
     cabinet = [int(1000 * random.random()) for i in range(size_of_cabinet)]
     global stepcounter
@@ -38,16 +33,35 @@ def check_steps(size_of_cabinet):
     insertion_sort(cabinet)
     return stepcounter
 
+random.seed(5040)
+xs = list(range(1,100))
+ys = [check_steps(x) for x in xs]
+ys_exp = [math.exp(x) for x in xs]
+plt.plot(xs,ys)
+axes = plt.gca()
+axes.set_ylim([np.min(ys),np.max(ys) + 140])
+plt.plot(xs,ys_exp)
+plt.title('Comparing Insertion Sort to the Exponential Function')
+plt.xlabel('Number of Files in Random Cabinet')
+plt.ylabel('Steps Required to Sort Cabinet')
+# plt.show()
 
 random.seed(5040)
 xs = list(range(1,100))
 ys = [check_steps(x) for x in xs]
-print(ys)
-
+xs_exp = [math.exp(x) for x in xs]
+xs_squared = [x**2 for x in xs]
+xs_threehalves = [x**1.5 for x in xs]
+xs_cubed = [x**3 for x in xs]
 plt.plot(xs,ys)
-plt.title('Количество шагов, необходимых для сортировки\nметодом вставки для случайных полок')
-plt.xlabel('Количество папок на случайной полке')
-plt.ylabel('Количество шагов, необходимых\nдля сортировки методом вставки')
+axes = plt.gca()
+axes.set_ylim([np.min(ys),np.max(ys) + 140])
+plt.plot(xs,xs_exp)
+plt.plot(xs,xs)
+plt.plot(xs,xs_squared)
+plt.plot(xs,xs_cubed)
+plt.plot(xs,xs_threehalves)
+plt.title('Comparing Insertion Sort to Other Growth Rates')
+plt.xlabel('Number of Files in Random Cabinet')
+plt.ylabel('Steps Required to Sort Cabinet')
 plt.show()
-
-
